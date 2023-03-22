e5806f524b3b0423c53d12b62efa9776c39a4051
unclejames2610 <udealaebuka@gmail.com>
2023-03-22 17:50:28 +0100

    navbar-top

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { AiOutlineMenu } from "react-icons/ai";
+import { AiOutlineMenu, AiOutlineSearch } from "react-icons/ai";
+import { BsFillCartFill } from "react-icons/bs";
 
 const Navbar = () => {
   return (
@@ -9,14 +10,29 @@ const Navbar = () => {
         <div className="cursor-pointer">
           <AiOutlineMenu size={30} />
         </div>
-        <h1>
-          Best <span>Eats</span>
+        <h1 className="text-2xl sm:text-3xl lg:text-4xl px-2">
+          Best <span className="font-bold">Eats</span>
         </h1>
-        <div>
-          <p>Delivery</p>
-          <p>Pickup</p>
+        <div className="hidden lg:flex items-center bg-gray-200 rounded-full p-1 text-[14px]">
+          <p className="bg-black text-white rounded-full p-2">Delivery</p>
+          <p className="p-2">Pickup</p>
         </div>
       </div>
+
+      {/* Search Input */}
+
+      <div className="bg-gray-200 rounded-full flex items-center px-2 w-[200px] sm:w-[400px] lg:w-[500px]">
+        <AiOutlineSearch size={25} />
+        <input
+          type="text"
+          placeholder="Search foods"
+          className="bg-transparent p-2 w-full focus:outline-none"
+        />
+      </div>
+      {/* Cart button */}
+      <button className="bg-black text-white hidden md:flex items-center py-2 rounded-full">
+        <BsFillCartFill size={20} className="mr-2" /> Cart
+      </button>
     </div>
   );
 };
