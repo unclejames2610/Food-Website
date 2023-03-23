eb844fbe0e38b163c00a4346ac044145ea3c816a
unclejames2610 <udealaebuka@gmail.com>
2023-03-23 15:24:03 +0100

    foodfilter

--- src/components/Headinecards.jsx
+++ src/components/Headinecards.jsx
@@ -9,7 +9,7 @@ const Headinecards = () => {
         <div className="absolute w-full h-full bg-black/50 rounded-xl text-white">
           <p className="font-bold text-2xl px-2 pt-4">Sun's Out, BOGO's Out</p>
           <p className="px-2">Through 8/26</p>
-          <button className="border-white bg-white text-black mx-2 absolute bottom-4">
+          <button className="border-white bg-white text-black mx-2 absolute bottom-4 hover:bg-transparent/70 hover:text-white">
             Order Now
           </button>
         </div>
@@ -25,7 +25,7 @@ const Headinecards = () => {
         <div className="absolute w-full h-full bg-black/50 rounded-xl text-white">
           <p className="font-bold text-2xl px-2 pt-4">New Restaurants</p>
           <p className="px-2">Added Daily</p>
-          <button className="border-white bg-white text-black mx-2 absolute bottom-4">
+          <button className="border-white bg-white text-black mx-2 absolute bottom-4 hover:bg-transparent/70 hover:text-white">
             Order Now
           </button>
         </div>
@@ -43,7 +43,7 @@ const Headinecards = () => {
             We Deliver Desserts Too
           </p>
           <p className="px-2">Tasty Treats</p>
-          <button className="border-white bg-white text-black mx-2 absolute bottom-4">
+          <button className="border-white bg-white text-black mx-2 absolute bottom-4 hover:bg-transparent/70 hover:text-white">
             Order Now
           </button>
         </div>
