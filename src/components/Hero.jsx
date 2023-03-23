d199fe543c853eb929695db948e57f19a08ebf1b
unclejames2610 <udealaebuka@gmail.com>
2023-03-23 16:19:43 +0100

    done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,6 +16,7 @@ const Hero = () => {
         <img
           className="w-full max-h-[500px] object-cover"
           src="https://images.pexels.com/photos/1639562/pexels-photo-1639562.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt="/"
         />
       </div>
     </div>
