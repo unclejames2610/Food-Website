ae468c1d3ca4f80d67349b1e2513e8e27a1b9329
Ameh Solomon Onyeke <amehsolomon46@gmail.com>
2023-04-03 17:36:12 +0100

    Worked on darkmode :)

--- src/App.js
+++ src/App.js
@@ -5,16 +5,23 @@ import Footer from "./components/Footer";
 import Headinecards from "./components/Headinecards";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import { useState } from "react";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+  const toggleDarkMode = () => {
+    setDarkMode((darkMode) => !darkMode);
+  };
   return (
-    <div>
-      <Navbar />
-      <Hero />
-      <Headinecards />
-      <Food />
-      <Category />
-      <Footer />
+    <div className={`${darkMode ? "dark" : ""}`}>
+      <div className="dark:bg-[#0c0500]">
+        <Navbar toggleDarkMode={toggleDarkMode} darkMode={darkMode} />
+        <Hero />
+        <Headinecards />
+        <Food />
+        <Category />
+        <Footer />
+      </div>
     </div>
   );
 }
